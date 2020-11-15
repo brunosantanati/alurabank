bcb179f5546eaf66459bea143211b39df39605cb
Bruno Rafael Sant Ana <bruno.santana.ti@gmail.com>
2020-11-15 16:02:30 -0300

    Criado controller para negociacao.

--- app/js/app.js
+++ app/js/app.js
@@ -1,4 +1,4 @@
-const negociacao = new Negociacao(new Date(), 1, 100);
-//negociacao._quantidade = 3; //agora que a propriedade eh private, gera erro de compilacao essa linha: Property '_quantidade' is private and only accessible within class 'Negociacao'.ts(2341)
-console.log(negociacao);
-console.log(negociacao.quantidade);
+const controller = new NegociacaoController();
+document
+    .querySelector('.form')
+    .addEventListener('submit', controller.adiciona.bind(controller));
