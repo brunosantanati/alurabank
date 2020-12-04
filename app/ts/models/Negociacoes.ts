5b1b65401a997c89d3ec8de53cef6371afb603e1
Bruno Rafael Sant Ana <bruno.santana.ti@gmail.com>
2020-12-04 18:33:07 -0300

    Deixar explicito o tipo de retorno dos metodos.

--- app/ts/models/Negociacoes.ts
+++ app/ts/models/Negociacoes.ts
@@ -2,11 +2,11 @@ class Negociacoes {
     //private _negociacoes: Array<Negociacao> = []; //primeira forma de declarar Array
     private _negociacoes: Negociacao[] = []; //outra forma de declarar o Array
 
-    adiciona(negociacao: Negociacao){
+    adiciona(negociacao: Negociacao): void{
         this._negociacoes.push(negociacao);
     }
 
-    paraArray(){
+    paraArray(): Negociacao[]{
         return [].concat(this._negociacoes);
     }
 }
