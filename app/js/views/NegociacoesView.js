dd50b9309d108d9e27a7814c8465ecc3b2bc677d
Bruno Rafael Sant Ana <bruno.santana.ti@gmail.com>
2020-12-06 14:26:40 -0300

    Simplificar codigo(remover return e chaves).

--- app/js/views/NegociacoesView.js
+++ app/js/views/NegociacoesView.js
@@ -19,16 +19,14 @@ class NegociacoesView {
 
             <tbody>
             </tbody>
-                ${model.paraArray().map(negociacao => {
-            return `
+                ${model.paraArray().map(negociacao => `
                     <tr>
                         <td>${negociacao.data.getDate() / negociacao.data.getMonth() + 1 / negociacao.data.getFullYear()}</td>
                         <td>${negociacao.quantidade}</td>
                         <td>${negociacao.valor}</td>
                         <td>${negociacao.volume}</td>
                     </tr>
-                    `;
-        }).join()}
+                    `).join()}
             <tfoot>
             </tfoot>
         </table>
