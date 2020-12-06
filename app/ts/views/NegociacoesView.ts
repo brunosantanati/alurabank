1c5397b297b209cf929fed88addca101429640a0
Bruno Rafael Sant Ana <bruno.santana.ti@gmail.com>
2020-12-06 14:29:20 -0300

    Ajuste para mostrar data corretamente.

--- app/ts/views/NegociacoesView.ts
+++ app/ts/views/NegociacoesView.ts
@@ -27,7 +27,7 @@ class NegociacoesView {
                 ${model.paraArray().map(negociacao => 
                     `
                     <tr>
-                        <td>${negociacao.data.getDate()/negociacao.data.getMonth()+1/negociacao.data.getFullYear()}</td>
+                        <td>${negociacao.data.getDate()}/${negociacao.data.getMonth()+1}/${negociacao.data.getFullYear()}</td>
                         <td>${negociacao.quantidade}</td>
                         <td>${negociacao.valor}</td>
                         <td>${negociacao.volume}</td>
